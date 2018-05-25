d642fb70c931a848827deef4b2adbe4c91e55287
Juan Carlos Coral <govindasd@gmail.com>
2018-05-25 16:51:54 -0300

    Layout pronto para desktop com algumas falhas em tela lg e media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  sidebarLinks = [
+    {name: "Painel de Gestão", iconUrl: "../assets/globe-white.svg", iconAltTxt: "icone de Globo"},
+    {name:"Conta Digital", iconUrl: "../assets/arroba.svg", iconAltTxt: "icone de arroba"},
+    {name:"Ant. de Recebíveis", iconUrl: "../assets/bar-graph.svg", iconAltTxt: "icone de Gráfico de Barras"}
+  ];
+  acoesAlertaNotas = [
+    {name:"Ação 1", actionUrl: "#"}
+    {name:"Ação 2", actionUrl: "#"}
+    {name:"Ação 3", actionUrl: "#"}
+  ];
+  acoesAlertaAssinaturas = [
+    {name:"Ação 1", actionUrl: "#"}
+    {name:"Ação 2", actionUrl: "#"}
+    {name:"Ação 3", actionUrl: "#"}
+  ]
 }
